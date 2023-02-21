85ba5b3e156dca284acc381c0cf641cf550f6815
oleksiiDranenko <alexdranenko2004@icloud.com>
2023-02-21 20:38:51 +0000

    display 'my posts'

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,12 +1,11 @@
 import classes from './Main.module.css';
 import { Post } from '../../components/post/Post';
-import photo from '../../icons/user-picture.png';
 
 import { getDocs, collection, query, orderBy, limit } from 'firebase/firestore';
 import { database } from '../../config/firebase';
 import { useEffect, useState } from 'react';
 
-interface Post {
+export interface PostInterface {
     postId: string,
     id: string,
     userPhoto: string,
@@ -15,15 +14,15 @@ interface Post {
 }
 
 export const Main = () => {
-    const [postsList, setPostsList] = useState<Post[] | null>(null)
+    const [postsList, setPostsList] = useState<PostInterface[] | null>(null);
 
     const postsCollection = collection(database, 'posts');
 
     const getPosts = async () => {
-        // Query for the 10 most recent posts
+        // query for the 10 most recent posts
         const q = query(postsCollection, orderBy("createdAt", "desc"), limit(10));
         const querySnapshot = await getDocs(q);
-        const posts = querySnapshot.docs.map((doc) => ({ ...doc.data(), postId: doc.id })) as Post[];
+        const posts = querySnapshot.docs.map((doc) => ({ ...doc.data(), postId: doc.id })) as PostInterface[];
 
         setPostsList(posts);
     }
--- src/pages/my-posts/MyPosts.tsx
+++ src/pages/my-posts/MyPosts.tsx
@@ -1,10 +1,44 @@
 import classes from './MyPosts.module.css';
+import { PostInterface } from '../main/Main';
+
+import { Post } from '../../components/post/Post';
+
+import { getDocs, collection, query, orderBy, limit } from 'firebase/firestore';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { database, auth } from '../../config/firebase';
+import { useEffect, useState } from 'react';
 
 export const MyPosts = () => {
+    //getting the user
+    const [user] = useAuthState(auth);
+
+    const [postsList, setPostsList] = useState<PostInterface[] | null>(null)
+
+    const postsCollection = collection(database, 'posts');
+
+    const getPosts = async () => {
+        // query for the 10 most recent posts
+        const q = query(postsCollection, orderBy("createdAt", "desc"), limit(10));
+        const querySnapshot = await getDocs(q);
+        const posts = querySnapshot.docs.map((doc) => ({ ...doc.data(), postId: doc.id })) as PostInterface[];
+
+        setPostsList(posts);
+    }
+    
+    useEffect(() => {
+        getPosts();
+    }, [])
 
     return (
         <div className={classes.page}>
-
+            <div className={classes.topDiv}></div>
+            <div className={classes.postsDiv}>
+            {postsList?.map((post) => {
+                if(user?.uid === post.id) {
+                    return <Post username={post.username} userPhoto={post.userPhoto} value={post.value} key={post.postId}/> 
+                }
+            })}
+            </div>
         </div>
     )
 }
