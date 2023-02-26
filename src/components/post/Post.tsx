254f20623791c0a638dd02bfd87a764027615f68
oleksiiDranenko <alexdranenko2004@icloud.com>
2023-02-26 17:02:28 +0000

    added ability to like the post and optimisation

--- src/components/post/Post.tsx
+++ src/components/post/Post.tsx
@@ -2,12 +2,18 @@
 import classes from './Post.module.css';
 //react hooks
 import { useState, useRef, useEffect } from 'react';
+//firebase
+import { addDoc, collection, deleteDoc, doc, getDocs, query, where } from 'firebase/firestore';
+import { auth, database } from '../../config/firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
 //copy
 import copy from 'copy-to-clipboard';
 //months array
 import { monthsArray } from '../../months/months-array';
-//icon
-import optionsIcon from '../../icons/options.png'
+//icons
+import optionsIcon from '../../icons/options.png';
+import likeIcon from './icons/like.png';
+import likeActiveIcon from './icons/like-active.png';
 
 
 interface PostInterface {
@@ -20,12 +26,81 @@ interface PostInterface {
     postId: string
 }
 
+interface LikeInterface {
+    userId: string
+}
+
 export const Post = (props: PostInterface) => {
+    //getting the user
+    const [user] = useAuthState(auth);
+
     //options state
     const [isOpen, setIsOpen] = useState<boolean>(false);
     const optionsRef = useRef<HTMLDivElement>(null);
     //copied state
     const [isCopied, setIsCopied] = useState<boolean>(false);
+    //likes count
+    const [likesArray, setLikesArray] = useState<LikeInterface[]>([]);
+    
+    //likes collection
+    const likesCollection = collection(database, 'likes');
+    //query for documents with this posts id
+    const likeDocs = query(likesCollection, where('postId', '==', props.postId));
+
+    //is user already liked 
+    const isLiked = likesArray.find((like) => like.userId === user?.uid);
+
+    //getting the likes array
+    const getLikes = async () => {
+        const data = await getDocs(likeDocs);
+        setLikesArray(data.docs.map((doc) => ({userId: doc.data().userId})))
+    }
+
+    //adding like document 
+    const addLike = async () => {
+        //change the state only if document is added to database
+        try {
+            await addDoc(likesCollection, {
+                userId: user?.uid,
+                postId: props.postId
+            })
+            if(user){
+                //changing the state
+                setLikesArray((likes) => likes ? [...likes, {userId: user?.uid}] : [{userId: user?.uid}])
+            }
+        } catch(err) {
+            console.error('Error: ', err)
+        }
+    }
+
+    //deleting like document 
+    const removeLike = async() => {
+        try {
+            //query for doc with the same post and user id
+            const deleteQuery = query(
+                likesCollection, 
+                where('postId', '==', props.postId), 
+                where('userId', '==', user?.uid)
+            );
+
+            //getting all documents with the query
+            const deleteData = await getDocs(deleteQuery);
+            //getting the first and only documents id
+            const deleteLike = doc(database, 'likes', deleteData.docs[0].id);
+            await deleteDoc(deleteLike);
+
+            //changing the state
+            setLikesArray(likesArray.filter((like) => like.userId !== user?.uid));
+        } catch(err) {
+            console.log('Error: ', err)
+        }
+    }
+
+    //update likes count state
+    useEffect(() => {
+        getLikes()
+    }, [])
+
 
     //track clicks outside options to close
     useEffect(() => {
@@ -91,10 +166,19 @@ export const Post = (props: PostInterface) => {
             <div className={classes.postValue}>
                 {props.value}
             </div>
-            <div className={classes.postDate}>
-                {`${props.date?.substring(11,16)} 
-                ${monthsArray[parseInt(props.date?.substring(5,8))]} 
-                ${props.date?.substring(8,10)}`} 
+            <div className={classes.footer}>
+                <div className={classes.postDate}>
+                    {`${props.date?.substring(11,16)} 
+                    ${monthsArray[parseInt(props.date?.substring(5,8))]} 
+                    ${props.date?.substring(8,10)}`} 
+                </div>
+
+                <div className={classes.likesSection}>
+                    <button onClick={isLiked ? removeLike : addLike} className={classes.likeButton}>
+                        <img src={isLiked ? likeActiveIcon : likeIcon} className={classes.likeIcon}/>
+                    </button>
+                    <span className={classes.likesCount}>{likesArray.length}</span>
+                </div>
             </div>
         </div>
     )
--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -3,7 +3,7 @@ import classes from './Main.module.css';
 //custom elements
 import { Post } from '../../components/post/Post';
 //firebase
-import { getDocs, collection, query, orderBy, limit, doc, deleteDoc } from 'firebase/firestore';
+import { getDocs, collection, query, orderBy, limit, doc, deleteDoc, where, writeBatch } from 'firebase/firestore';
 import { database, auth } from '../../config/firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
 //react hooks
@@ -35,13 +35,15 @@ export const Main = () => {
     const dispatch = useDispatch();
 
     const postsCollection = collection(database, 'posts');
+    const likesCollection = collection(database, 'likes');
 
     const getPosts = async () => {
-        // query for the 10 most recent posts
+        //query for the 10 most recent posts
         const q = query(postsCollection, orderBy("createdAt", "desc"), limit(10));
         const querySnapshot = await getDocs(q);
         const posts = querySnapshot.docs.map((doc) => ({ ...doc.data(), postId: doc.id })) as PostInterface[];
 
+        //update the state of posts array
         dispatch(getPostsArray(posts));
         setIsLoading(false);
     }
@@ -52,8 +54,20 @@ export const Main = () => {
 
     //delete post in database
     const deletePost = async (post: PostInterface) => {
+        //deleting post in 'posts' collection
         const postRef = doc(database, 'posts', post.postId);
         await deleteDoc(postRef);
+
+        //deleting all likes of the post
+        //query for doc with the same post and user id
+        const deleteQuery = query(likesCollection, where('postId', '==', post.postId));
+        //getting all documents with the query
+        const deleteData = await getDocs(deleteQuery);
+        //create a batch
+        const batch = writeBatch(database);
+        deleteData.forEach(doc => batch.delete(doc.ref));
+        //commit a batch to delete all documents at once
+        await batch.commit();
     }
     //delete and make a request to change the posts array state
     const handleDelete = async (post: PostInterface) => {
@@ -65,7 +79,8 @@ export const Main = () => {
         <div className={classes.page}>
             <div className={classes.topDiv}></div>
             <div className={classes.postsDiv}>
-                {isLoading ? <Loading w='50px' h='50px'/> : postsArray?.map((post) => {
+                {isLoading ? <Loading w='50px' h='50px'/> 
+                : postsArray?.map((post) => {
                     return (
                     <Post
                         username={post.username} 
